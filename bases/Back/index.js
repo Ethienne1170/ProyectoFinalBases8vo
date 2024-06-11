b89ce78257e368dc09cadd2e877f55df648bd896
Joaquin Miguel Esquivel Meza <joaquin.miguel2002@gmail.com>
2024-06-11 17:45:27 -0600

    perfil

--- bases/Back/index.js
+++ bases/Back/index.js
@@ -16,6 +16,9 @@ app.use('/usuarios', plantas);
 app.use('/list_inver', plantas);
 app.use('/login', plantas);
 app.use('/usuario_info', plantas);
+app.use('/update_usuario', plantas);
+app.use('/update_usuario_pass', plantas);
+
 
 app.listen(3000,()=>{
     console.log('servidor corriendo en el puerto: 3000')
