49b183c7fe6e921f45195f56bc4889f390b89db3
Joaquin Miguel Esquivel Meza <joaquin.miguel2002@gmail.com>
2024-06-11 00:28:34 -0600

    muchas cosas de sesiones y admin

--- bases/Back/routes/PlantasRoutes.js
+++ bases/Back/routes/PlantasRoutes.js
@@ -35,6 +35,22 @@ router.get('/invernaderos', (req, res)=>{
     }
 })
 
+router.get('/list_inver', (req, res)=>{
+    try {
+        db.query('SELECT i.claveusuario, CONCAT(u.nombre, " ", u.appaterno) AS nUsuario, i.clave, i.nombre, i.numeroext, i.numeroint, i.calle, i.estado, i.region ' + 
+                    'FROM invernadero i INNER JOIN usuario u ON i.claveusuario=u.clave', (error, result)=>{
+            if (error) {
+                res.json(error);
+                return;
+            }
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        console.log(error)
+        res.json(error);
+    }
+})
+
 router.get('/usuarios', (req, res)=>{
     try {
         db.query('SELECT * FROM usuario', (error, result)=>{
@@ -52,7 +68,7 @@ router.get('/usuarios', (req, res)=>{
 
 router.get('/login', (req, res)=>{
     try {
-        db.query('SELECT usuario, contrasena FROM usuario', (error, result)=>{
+        db.query('SELECT usuario, contrasena, privilegios FROM usuario', (error, result)=>{
             if (error) {
                 res.json(error);
                 return;
